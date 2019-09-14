0912b21c8ad5f34ce23de5558ae7fc0782132915
Chol Nhial <chol.nhial.chol@gmail.com>
2019-09-14 11:24:36 +1000

    initial commit

---
+++ src/main/java/com/cholnhial/ireviewmovies/IreviewmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.cholnhial.ireviewmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IreviewmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IreviewmoviesApplication.class, args);
+	}
+
+}
