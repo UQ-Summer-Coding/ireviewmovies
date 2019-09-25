c3694284c3496c5fc892e83edecfe90a846a1913
Chol Nhial <chol.nhial.chol@gmail.com>
2019-09-26 00:50:26 +1000

    Use existing roles when mapping UserSignUpDTO to User entity

--- src/main/java/com/cholnhial/ireviewmovies/service/mapper/UserMapper.java
+++ src/main/java/com/cholnhial/ireviewmovies/service/mapper/UserMapper.java
@@ -3,20 +3,27 @@ package com.cholnhial.ireviewmovies.service.mapper;
 import com.cholnhial.ireviewmovies.model.Role;
 import com.cholnhial.ireviewmovies.model.User;
 import com.cholnhial.ireviewmovies.model.enumeration.RoleType;
+import com.cholnhial.ireviewmovies.repository.RoleRepository;
 import com.cholnhial.ireviewmovies.service.dto.UserSignUpDTO;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Service
+@AllArgsConstructor
 public class UserMapper {
+
+    private final RoleRepository roleRepository;
 
     public User UserSignUpDTOToUser(UserSignUpDTO userSignUpDTO) {
         User user = new User();
         user.setEmail(userSignUpDTO.getEmail());
         user.setFullName(userSignUpDTO.getFullName());
         user.setPassword(userSignUpDTO.getPassword());
-        user.setRoles(Set.of(new Role(RoleType.ROLE_USER)));
+        Optional<Role> userRole = roleRepository.findByRole(RoleType.ROLE_USER);
+        userRole.ifPresent(r -> user.setRoles(Set.of(r)));
 
         return user;
     }
