5edbad65c018f71e02d056a7417fc7f4462261bb
Chol Nhial <chol.nhial.chol@gmail.com>
2019-11-29 10:34:53 +1000

    Show user star rating and also show a loader when loading details for movie review

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
 
 
 function openMovieReviewsDetailsModal(movieReviewId) {
+    $.LoadingOverlay("show");
     $.ajax({
         url: "/user/my-reviews/" + movieReviewId,
         success: function (data) {
             $('#movieReviewDetailsModalHolder').html(data);
+            $.LoadingOverlay("hide");
             $('#movieReviewDetailsModal').modal('show');
+        },
+        error: function(data) {
+            $.LoadingOverlay("hide");
         }
     })
 }
