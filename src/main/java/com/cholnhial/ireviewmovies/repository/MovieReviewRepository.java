468cf0dad9c4521d5e135c79272b097f9e0dbe6a
Chol Nhial <chol.nhial.chol@gmail.com>
2020-08-01 11:05:13 +1000

    Add a method to MovieReviewRepository to return average reviews for all users for a given movie

--- src/main/java/com/cholnhial/ireviewmovies/repository/MovieReviewRepository.java
+++ src/main/java/com/cholnhial/ireviewmovies/repository/MovieReviewRepository.java
@@ -4,9 +4,15 @@ import com.cholnhial.ireviewmovies.model.MovieReview;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieReviewRepository extends JpaRepository<MovieReview, Long> {
     Page<MovieReview> findAllByUserId(Long userId, Pageable pageable);
+
+    @Query("select avg(m.rating) from MovieReview m " +
+            "where m.tMDBMovieId = :movieId group by m.tMDBMovieId")
+    Float getAverageRatingByTMDBMovieId(@Param("movieId") Long TMdbMovieId);
 }
