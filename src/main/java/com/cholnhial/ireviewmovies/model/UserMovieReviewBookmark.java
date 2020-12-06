cb7f1a7cc7364ca5776b8ca6f950a83156866707
leecwj <joshuacwlee23@gmail.com>
2020-12-06 15:16:07 +1000

    add columns and entity associations to user bookmark model

--- src/main/java/com/cholnhial/ireviewmovies/model/UserMovieReviewBookmark.java
+++ src/main/java/com/cholnhial/ireviewmovies/model/UserMovieReviewBookmark.java
@@ -1,4 +1,29 @@
 package com.cholnhial.ireviewmovies.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "user_movie_review_bookmark")
+@Data
+@EqualsAndHashCode(of = {"id"})
 public class UserMovieReviewBookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "movie_review_id", referencedColumnName = "id")
+    private MovieReview movieReview;
+
+    @Column(name = "created")
+    private ZonedDateTime created;
 }
---
+++ src/main/resources/db/migration/V15__user_movie_review_bookmark_add_created_date_column.sql
@@ -0,0 +1,2 @@
+ALTER TABLE user_movie_review_bookmark
+ADD COLUMN `created` datetime NOT NULL;
