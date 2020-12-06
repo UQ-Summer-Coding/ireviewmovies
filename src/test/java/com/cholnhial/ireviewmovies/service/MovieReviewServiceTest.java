9902860b5c8e81c7753a65795976fc099d6eb373
Chol Nhial <chol.nhial.chol@gmail.com>
2020-12-06 17:37:10 +1000

    Create a sample test file for testing MovieReviewService

---
+++ src/test/java/com/cholnhial/ireviewmovies/service/MovieReviewServiceTest.java
@@ -0,0 +1,20 @@
+package com.cholnhial.ireviewmovies.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewServiceTest {
+
+    @Autowired
+    private MovieReviewService movieReviewService;
+
+    @Test
+    void findAllByUserId() {
+    }
+
+    @Test
+    void deleteMovieReviewById() {
+    }
+}
