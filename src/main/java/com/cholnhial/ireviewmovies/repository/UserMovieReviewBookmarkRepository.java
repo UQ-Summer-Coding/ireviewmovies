04b972ef4a8223469ad239e18a932f62e783154c
leecwj <joshuacwlee23@gmail.com>
2020-12-29 16:25:00 +1000

    added method to find bookmark by user id and review id

--- src/main/java/com/cholnhial/ireviewmovies/repository/UserMovieReviewBookmarkRepository.java
+++ src/main/java/com/cholnhial/ireviewmovies/repository/UserMovieReviewBookmarkRepository.java
@@ -2,12 +2,20 @@ package com.cholnhial.ireviewmovies.repository;
 
 
 import com.cholnhial.ireviewmovies.model.UserMovieReviewBookmark;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface UserMovieReviewBookmarkRepository extends JpaRepository<UserMovieReviewBookmark, Long> {
-    Set<UserMovieReviewBookmark> findAllByUserId(Long userId);
+    Page<UserMovieReviewBookmark> findAllByUserId(Long userId, Pageable pageable);
+
+    @Query("select umrb from UserMovieReviewBookmark as umrb WHERE umrb.user.id=:userId and umrb.movieReview.id=:reviewId")
+    Optional<UserMovieReviewBookmark> findOneByUserIdAndReviewId(@Param("userId") Long userId,@Param("reviewId") Long reviewId);
 }
