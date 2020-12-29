475a110e191e00bf7cf5b4fbecc5f26af3e7dd28
leecwj <joshuacwlee23@gmail.com>
2020-12-29 16:20:38 +1000

    created exceptions for bookmark review service

---
+++ src/main/java/com/cholnhial/ireviewmovies/service/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cholnhial.ireviewmovies.service.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
